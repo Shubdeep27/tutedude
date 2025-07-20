4adde765ff08b11070726f3725c61ce150c3a833
Shubdeep27 <shubhamshanu143@gmail.com>
2025-07-20 21:01:05 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,17 @@
+#ASSIGNMENT 1
+a = float(input ("Enter the first number:  "))
+b = float(input ("Enter the second number: "))
+
+c= a+b
+d=  a-b
+e = a*b
+f = a/b
+
+print("Addition:  ", c)
+print("Subtraction:  ",d)
+print("Multiplication:  ",e)
+print("Division:  ",f)
+#Assignment 2
+p = input("Enter your first name:  ")
+q = input("Enter your second name:  ")
+print("Hello, ",p, q,"! Welcome to the Python program.")
