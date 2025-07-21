9aef615621330fdfa30e21278db973c7148546f8
Shubdeep27 <shubhamshanu143@gmail.com>
2025-07-21 11:39:19 +0530

    Add files via upload

---
+++ assig_2.py
@@ -0,0 +1,16 @@
+#assignment 2 Task 1
+x=int(input("Enter a number: "))
+
+if (x/2) == x//2:
+    print(x," is a even number")
+else:
+    print(x, " is a odd number")
+
+print("thank you")
+
+
+#Assignment 2 Task 2
+p = 0
+for i in range(1,51):
+    p += i 
+print  ("the sum of toal of 1 to 50 is :",p)
